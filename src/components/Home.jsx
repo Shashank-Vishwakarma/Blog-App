e3cbad23f21529934b500fca76045f987cfa474f
shashank <shashank78680@gmail.com>
2024-02-18 15:25:36 +0530

    minor change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
     return (
         <div className="px-4 py-4 text-center font-bold text-3xl align-middle">
-            Your Posts
+            Please login to see your posts
         </div>
     )
 }
